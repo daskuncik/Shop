5d5b4cf1071f57fc81e3bb3f0245afdd8d2bc215
Solomatina Daria <daskuncik@gmail.com>
2017-12-02 14:10:29 +0300

    Middleware added

---
+++ Shop_new/Shop_new/CustomAuthorisation/CustomAuthorizationMiddleware.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Shop_new.CustomAuthorisation
+{
+    public class CustomAuthorizationMiddleware
+    {
+        public static string AuthorizationWord = "Authorization";
+        public static string UserWord = "User";
+        protected readonly RequestDelegate _next;
+        protected readonly TokenStore tokensStore;
+        //////?????
+        
+
+        public CustomAuthorizationMiddleware(RequestDelegate next, TokenStore tokensStore)
+        {
+            _next = next;
+            this.tokensStore = tokensStore;
+        }
+
+        public virtual async Task Invoke(HttpContext context)
+        {
+            if (context.Request.Cookies.Keys.Contains(AuthorizationWord))
+            {
+                var auth = context.Request.Cookies[AuthorizationWord];
+                await CheckAuthorization(context, auth);
+            }
+            else if (context.Request.Path.Value.Split('/').Intersect(GetAnonymousPaths()).Any())
+            {
+                await this._next(context);
+            }
+            else
+            {
+                var message = "No authorization header provided";
+                await ReturnForbidden(context, message);
+            }
+        }
+
+        protected async Task CheckAuthorization(HttpContext context, string auth)
+        {
+            var match = Regex.Match(auth, @"Bearer (\S+)");
+            if (match.Groups.Count == 1)
+            {
+                await ReturnForbidden(context, "Invalid token format");
+            }
+            else
+            {
+                var token = match.Groups[1].Value;
+                var result = tokensStore.CheckToken(token);
+                if (result == CheckTokenResult.Valid)
+                {
+                    context.Request.Headers.Add(UserWord, tokensStore.GetNameByToken(token));
+                    await this._next(context);
+                }
+                else if (result == CheckTokenResult.Expired)
+                    await ReturnForbidden(context, "Token has expired");
+                else
+                    await ReturnForbidden(context, "Token not valid");
+            }
+        }
+
+        public abstract Task ReturnForbidden(HttpContext context, string message);
+
+        public abstract List<string> GetAnonymousPaths();
+    }
+}
---
+++ Shop_new/Shop_new/CustomAuthorisation/ShopnewCustomAuthorizationMiddleware.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace Shop_new.CustomAuthorisation
+{
+    public class ShopnewCustomAuthorizationMiddleware : CustomAuthorizationMiddleware
+    {
+        public override List<string> GetAnonymousPaths() => new[] { "api", "auth", "login", "register" }.ToList();
+
+        public ShopnewCustomAuthorizationMiddleware(RequestDelegate next, TokenStore tokensStore) : base(next, tokensStore)
+        {
+        }
+
+        public override async Task Invoke(HttpContext context)
+        {
+            if (context.Request.Headers.Keys.Contains(AuthorizationWord))
+            {
+                await this._next(context);
+            }
+            else
+                await base.Invoke(context);
+        }
+
+        public override async Task ReturnForbidden(HttpContext context, string message)
+        {
+            string redirect = "/users/auth";
+            if (context.Request.Path != redirect)
+            {
+                redirect += $"?Redirect={context.Request.Path}";
+            }
+            context.Response.Redirect(redirect);
+        }
+    }
+}
